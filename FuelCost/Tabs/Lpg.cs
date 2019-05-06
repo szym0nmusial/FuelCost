c2ece9eaf0af1c7e222dc942a4848f8c8a0e6734
Szymon <szymon_0@hotmail.com>
2019-05-06 22:59:03 +0200

    Działające UI do lpg

--- FuelCost/Tabs/Lpg.cs
+++ FuelCost/Tabs/Lpg.cs
@@ -19,6 +19,10 @@ namespace FuelCost
 
         public VehicleData Data;
 
+        private EditText Cost;
+        private EditText Distance;
+        private float price = 2.08f;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -29,12 +33,39 @@ namespace FuelCost
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             // Use this to return your custom view for this Fragment
-             return inflater.Inflate(Resource.Layout.lpg, container, false);
+
+            var view =  inflater.Inflate(Resource.Layout.lpg, container, false);
+
+            view.FindViewById<TextView>(Resource.Id.name).Text = Data?.Name;
+            view.FindViewById<TextView>(Resource.Id.typ).Text = Data?.FuelType.ToString();
+            view.FindViewById<TextView>(Resource.Id.cons).Text = Data?.consumption.ToString();
+            view.FindViewById<TextView>(Resource.Id.price).Text = "2,08";
+
+            Distance = view.FindViewById<EditText>(Resource.Id.distance);
+            Distance.TextChanged += Distance_Changed;
+            Cost = view.FindViewById<EditText>(Resource.Id.cost);
+            Cost.TextChanged += Cost_Changed;
+
+            return view;
 
            // return base.OnCreateView(inflater, container, savedInstanceState);
         }
 
-     
+        private void Cost_Changed(object sender, Android.Text.TextChangedEventArgs e)
+        {
+           // string tmp = 
 
+           // Distance.Text =  e.Text
+        }
+
+        private void Distance_Changed(object sender, Android.Text.TextChangedEventArgs e)
+        {
+            try
+            {
+                var tmp = Data.consumption * price * 0.01f * float.Parse(Distance.Text);
+                Cost.Text = tmp.ToString();
+            }
+            catch { }
+        }
     }
 }
