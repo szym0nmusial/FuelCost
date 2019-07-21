c90b2479e0e5a3fc514ba5d261c4dbdda3363aa3
Laptop_Szymon <szymon_0@hotmail.com>
2019-07-22 00:09:36 +0200

    tlo ikonki

--- FuelCost/Resources/Resource.Designer.cs
+++ FuelCost/Resources/Resource.Designer.cs
@@ -1239,44 +1239,44 @@ namespace FuelCost
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f0a0058
-			public const int abc_background_cache_hint_selector_material_dark = 2131361880;
-			
 			// aapt resource value: 0x7f0a0059
-			public const int abc_background_cache_hint_selector_material_light = 2131361881;
+			public const int abc_background_cache_hint_selector_material_dark = 2131361881;
 			
 			// aapt resource value: 0x7f0a005a
-			public const int abc_btn_colored_borderless_text_material = 2131361882;
+			public const int abc_background_cache_hint_selector_material_light = 2131361882;
 			
 			// aapt resource value: 0x7f0a005b
-			public const int abc_btn_colored_text_material = 2131361883;
+			public const int abc_btn_colored_borderless_text_material = 2131361883;
 			
 			// aapt resource value: 0x7f0a005c
-			public const int abc_color_highlight_material = 2131361884;
+			public const int abc_btn_colored_text_material = 2131361884;
 			
 			// aapt resource value: 0x7f0a005d
-			public const int abc_hint_foreground_material_dark = 2131361885;
+			public const int abc_color_highlight_material = 2131361885;
 			
 			// aapt resource value: 0x7f0a005e
-			public const int abc_hint_foreground_material_light = 2131361886;
+			public const int abc_hint_foreground_material_dark = 2131361886;
+			
+			// aapt resource value: 0x7f0a005f
+			public const int abc_hint_foreground_material_light = 2131361887;
 			
 			// aapt resource value: 0x7f0a0004
 			public const int abc_input_method_navigation_guard = 2131361796;
 			
-			// aapt resource value: 0x7f0a005f
-			public const int abc_primary_text_disable_only_material_dark = 2131361887;
-			
 			// aapt resource value: 0x7f0a0060
-			public const int abc_primary_text_disable_only_material_light = 2131361888;
+			public const int abc_primary_text_disable_only_material_dark = 2131361888;
 			
 			// aapt resource value: 0x7f0a0061
-			public const int abc_primary_text_material_dark = 2131361889;
+			public const int abc_primary_text_disable_only_material_light = 2131361889;
 			
 			// aapt resource value: 0x7f0a0062
-			public const int abc_primary_text_material_light = 2131361890;
+			public const int abc_primary_text_material_dark = 2131361890;
 			
 			// aapt resource value: 0x7f0a0063
-			public const int abc_search_url_text = 2131361891;
+			public const int abc_primary_text_material_light = 2131361891;
+			
+			// aapt resource value: 0x7f0a0064
+			public const int abc_search_url_text = 2131361892;
 			
 			// aapt resource value: 0x7f0a0005
 			public const int abc_search_url_text_normal = 2131361797;
@@ -1287,29 +1287,29 @@ namespace FuelCost
 			// aapt resource value: 0x7f0a0007
 			public const int abc_search_url_text_selected = 2131361799;
 			
-			// aapt resource value: 0x7f0a0064
-			public const int abc_secondary_text_material_dark = 2131361892;
-			
 			// aapt resource value: 0x7f0a0065
-			public const int abc_secondary_text_material_light = 2131361893;
+			public const int abc_secondary_text_material_dark = 2131361893;
 			
 			// aapt resource value: 0x7f0a0066
-			public const int abc_tint_btn_checkable = 2131361894;
+			public const int abc_secondary_text_material_light = 2131361894;
 			
 			// aapt resource value: 0x7f0a0067
-			public const int abc_tint_default = 2131361895;
+			public const int abc_tint_btn_checkable = 2131361895;
 			
 			// aapt resource value: 0x7f0a0068
-			public const int abc_tint_edittext = 2131361896;
+			public const int abc_tint_default = 2131361896;
 			
 			// aapt resource value: 0x7f0a0069
-			public const int abc_tint_seek_thumb = 2131361897;
+			public const int abc_tint_edittext = 2131361897;
 			
 			// aapt resource value: 0x7f0a006a
-			public const int abc_tint_spinner = 2131361898;
+			public const int abc_tint_seek_thumb = 2131361898;
 			
 			// aapt resource value: 0x7f0a006b
-			public const int abc_tint_switch_track = 2131361899;
+			public const int abc_tint_spinner = 2131361899;
+			
+			// aapt resource value: 0x7f0a006c
+			public const int abc_tint_switch_track = 2131361900;
 			
 			// aapt resource value: 0x7f0a0008
 			public const int accent_material_dark = 2131361800;
@@ -1347,8 +1347,8 @@ namespace FuelCost
 			// aapt resource value: 0x7f0a0013
 			public const int bright_foreground_material_light = 2131361811;
 			
-			// aapt resource value: 0x7f0a0056
-			public const int button_checked = 2131361878;
+			// aapt resource value: 0x7f0a0057
+			public const int button_checked = 2131361879;
 			
 			// aapt resource value: 0x7f0a0014
 			public const int button_material_dark = 2131361812;
@@ -1356,8 +1356,8 @@ namespace FuelCost
 			// aapt resource value: 0x7f0a0015
 			public const int button_material_light = 2131361813;
 			
-			// aapt resource value: 0x7f0a0055
-			public const int button_pressed = 2131361877;
+			// aapt resource value: 0x7f0a0056
+			public const int button_pressed = 2131361878;
 			
 			// aapt resource value: 0x7f0a0000
 			public const int cardview_dark_background = 2131361792;
@@ -1380,14 +1380,14 @@ namespace FuelCost
 			// aapt resource value: 0x7f0a004c
 			public const int colorPrimaryDark = 2131361868;
 			
-			// aapt resource value: 0x7f0a0054
-			public const int control_border = 2131361876;
+			// aapt resource value: 0x7f0a0055
+			public const int control_border = 2131361877;
 			
 			// aapt resource value: 0x7f0a0040
 			public const int design_bottom_navigation_shadow_color = 2131361856;
 			
-			// aapt resource value: 0x7f0a006c
-			public const int design_error = 2131361900;
+			// aapt resource value: 0x7f0a006d
+			public const int design_error = 2131361901;
 			
 			// aapt resource value: 0x7f0a0041
 			public const int design_fab_shadow_end_color = 2131361857;
@@ -1413,8 +1413,8 @@ namespace FuelCost
 			// aapt resource value: 0x7f0a0048
 			public const int design_snackbar_background_color = 2131361864;
 			
-			// aapt resource value: 0x7f0a006d
-			public const int design_tint_password_toggle = 2131361901;
+			// aapt resource value: 0x7f0a006e
+			public const int design_tint_password_toggle = 2131361902;
 			
 			// aapt resource value: 0x7f0a0016
 			public const int dim_foreground_disabled_material_dark = 2131361814;
@@ -1443,8 +1443,11 @@ namespace FuelCost
 			// aapt resource value: 0x7f0a001e
 			public const int highlighted_text_material_light = 2131361822;
 			
-			// aapt resource value: 0x7f0a0057
-			public const int ic_launcher_background = 2131361879;
+			// aapt resource value: 0x7f0a0058
+			public const int ic_launcher_background = 2131361880;
+			
+			// aapt resource value: 0x7f0a0054
+			public const int icon_bg = 2131361876;
 			
 			// aapt resource value: 0x7f0a001f
 			public const int material_blue_grey_800 = 2131361823;
@@ -1545,11 +1548,11 @@ namespace FuelCost
 			// aapt resource value: 0x7f0a003a
 			public const int switch_thumb_disabled_material_light = 2131361850;
 			
-			// aapt resource value: 0x7f0a006e
-			public const int switch_thumb_material_dark = 2131361902;
-			
 			// aapt resource value: 0x7f0a006f
-			public const int switch_thumb_material_light = 2131361903;
+			public const int switch_thumb_material_dark = 2131361903;
+			
+			// aapt resource value: 0x7f0a0070
+			public const int switch_thumb_material_light = 2131361904;
 			
 			// aapt resource value: 0x7f0a003b
 			public const int switch_thumb_normal_material_dark = 2131361851;
